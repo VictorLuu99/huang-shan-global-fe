b57889cbf4f5ec3ae72e9943f1ebfdbb4084d0bd
VictorLuu <victorluu99@gmail.com>
2025-09-10 02:20:15 +0700

    remove component not used

--- src/components/shared/index.ts
+++ src/components/shared/index.ts
@@ -1,15 +1,4 @@
 // Shared Components
-export { default as SearchBar } from './SearchBar';
-export { default as ArticleCard } from './ArticleCard';
-export { default as Pagination } from './Pagination';
-export { default as CategoryFilter } from './CategoryFilter';
-export { default as LoadingSpinner } from './LoadingSpinner';
-export { default as Modal } from './Modal';
-export { default as Badge } from './Badge';
-export { default as Button } from './Button';
-export { default as Card } from './Card';
-export { default as FormInput } from './FormInput';
-export { default as FormTextarea } from './FormTextarea';
 export { default as Header } from './Header';
 
 // SEO utilities
